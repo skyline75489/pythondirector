551e1c7d98c32b73e971910a65d0febb2dd211c6
anthonybaxter <anthonybaxter>
2003-10-10 05:41:58 +0000

    new version

--- pydirector/__init__.py
+++ pydirector/__init__.py
@@ -2,9 +2,9 @@
 # Copyright (c) 2002-2003 ekit.com Inc (http://www.ekit-inc.com)
 # and Anthony Baxter <anthony@interlink.com.au>
 #
-# $Id: __init__.py,v 1.11 2003/10/09 08:53:42 anthonybaxter Exp $
+# $Id: __init__.py,v 1.12 2003/10/10 05:41:58 anthonybaxter Exp $
 #
 
 # Package init file.
 
-Version = '0.0.6'
+Version = '0.0.7'
